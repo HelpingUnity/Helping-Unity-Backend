84be2b057b508c09c3ea806bb92c4eb4819ad11b
M.M. Naja <mmnaja@gmail.com>
2024-09-11 22:52:50 +0530

    Initial commit

---
+++ src/main/java/com/helpingunity/helping_unity_backend/HelpingUnityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.helpingunity.helping_unity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingUnityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpingUnityBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpingunity/helping_unity_backend/HelpingUnityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpingunity.helping_unity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingUnityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
