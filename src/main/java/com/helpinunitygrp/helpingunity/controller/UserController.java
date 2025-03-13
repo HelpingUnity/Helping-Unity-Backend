5b28decb0eeecac6112e2eaad965d1c7831e2d6d
LinalFernando <linalfernando123@gmail.com>
2025-03-14 00:23:22 +0530

    Bug fix

--- src/main/java/com/helpinunitygrp/helpingunity/controller/UserController.java
+++ src/main/java/com/helpinunitygrp/helpingunity/controller/UserController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.helpinunitygrp.helpingunity.model.User;
-import com.helpinunitygrp.helpingunity.repository.Userrepository;
-import com.helpinunitygrp.helpingunity.security.JwtTokenProvider;
 import com.helpinunitygrp.helpingunity.service.UserService;
 
 @RestController
